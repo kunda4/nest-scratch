2886f46f3c6d2fadf8a55371b150ad9c78354d0f
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-05 18:20:25 +0200

    add route path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller('/app')
 export class AppController{
-    @Get()
+    @Get('/hi')
     getRootRouter(){
         return 'Hi There How are doing?'
+    }
+
+    @Get('bye')
+    getbyeRouter(){
+        return 'Bye Bye!!!'
     }
 }
